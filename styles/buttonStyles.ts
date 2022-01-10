4ceb265d95ba208d9a8f466e1ef8b4927b9da378
Jasper Baetens <jasper.baetens@jstack.eu>
2022-01-10 10:21:36 +0100

    initial commit

---
+++ styles/buttonStyles.ts
@@ -0,0 +1,17 @@
+export const buttonStyles = {
+  baseStyle: {},
+  sizes: {},
+  variants: {
+    primary: {
+      bg: 'primary.500',
+      color: 'white',
+      _hover: {
+        bg: 'primary.400',
+      },
+    },
+  },
+  defaultProps: {
+    size: 'md',
+    variant: '',
+  },
+}
